b46dc4a089a7ee7e02ff0558a222b7d0d16d7f34
mooinglemur <mooinglemur@users.noreply.github.com>
2023-05-15 06:58:24 +0000

    Set RTC weekday when using -rtc (#104)

--- src/rtc.c
+++ src/rtc.c
@@ -47,7 +47,7 @@ rtc_init(bool set_system_time)
 		seconds = tm.tm_sec;
 		minutes = tm.tm_min;
 		hours = tm.tm_hour;
-		day_of_week = 1;
+		day_of_week = (tm.tm_wday == 0 ? 7 : tm.tm_wday);
 		day = tm.tm_mday;
 		month = tm.tm_mon + 1;
 		year = tm.tm_year - 100;
