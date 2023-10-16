bf15b1e5d50de8db3c49a272c4262e67c64a2dce
mooinglemur <mooinglemur@users.noreply.github.com>
2023-10-15 22:38:04 -0600

    Prep for R45 (#177)

--- src/version.h
+++ src/version.h
@@ -2,6 +2,6 @@
 // Copyright (c) 2019, 2023 Michael Steil, et al
 // All rights reserved. License: 2-clause BSD
 
-#define VER "44"
-#define VER_NAME "Milan"
-#define VER_INFO "### Release 44 (\"Milan\")"
+#define VER "45"
+#define VER_NAME "Nuuk"
+#define VER_INFO "### Release 45 (\"Nuuk\")"
