c8c553ec8445f18c8f49fbd125bf6e9ede3906ac
DragWx <zazzal22@gmail.com>
2023-11-11 14:44:58 -0500

    Prevent IEEE intercept when hostfs device is not being addressed. (#194)

--- src/ieee.h
+++ src/ieee.h
@@ -4,12 +4,12 @@
 
 void ieee_init();
 int SECOND(uint8_t a);
-void TKSA(uint8_t a);
+int TKSA(uint8_t a);
 int ACPTR(uint8_t *a);
 int CIOUT(uint8_t a);
-void UNTLK(void);
+int UNTLK(void);
 int UNLSN(void);
-void LISTEN(uint8_t a);
-void TALK(uint8_t a);
+int LISTEN(uint8_t a);
+int TALK(uint8_t a);
 int MACPTR(uint16_t addr, uint16_t *count, uint8_t stream_mode);
 int MCIOUT(uint16_t addr, uint16_t *count, uint8_t stream_mode);
