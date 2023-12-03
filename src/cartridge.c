26883f0db64edf1f477d00026794eb4145c77238
Jason Thorpe <37479620+thorpej@users.noreply.github.com>
2023-12-03 01:13:04 -0800

    Cast "char" arguments to ctype(3) functions to "unsigned char" to avoid undefined behavior for negative char values. (#204)

--- src/cartridge.c
+++ src/cartridge.c
@@ -30,7 +30,7 @@ static int
 x16_strnicmp(char const *s0, char const *s1, int len)
 {
 	for(int i=0; (i<len) && (*s0 && *s1); ++i) {
-		if(tolower(*s0) != tolower(*s1)) {
+		if(tolower((unsigned char)*s0) != tolower((unsigned char)*s1)) {
 			break;
 		}
 		++s0;
@@ -216,7 +216,7 @@ static char *
 rtrim(char *str)
 {
     char *c = str + strlen(str) - 1;
-    while(isspace(*c)) {
+    while(isspace((unsigned char)*c)) {
 		--c;
 	}
     *(c + 1) = '\0';
