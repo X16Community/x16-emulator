afa6434900686c17e5a5253f168a74efa0cba45f
mooinglemur <mooinglemur@users.noreply.github.com>
2024-03-20 00:20:59 -0700

    set SDL2 dbus app name, pre-bump reported version (#255)
    * set SDL2 dbus app name, pre-bump reported version
    
    * ifdefs for support of old SDL versions

--- src/version.h
+++ src/version.h
@@ -1,7 +1,7 @@
 // Commander X16 Emulator
-// Copyright (c) 2019, 2023 Michael Steil, et al
+// Copyright (c) 2019, 2024 Michael Steil, et al
 // All rights reserved. License: 2-clause BSD
 
-#define VER "46"
-#define VER_NAME "Winnipeg"
-#define VER_INFO "### Release 46 (\"Winnipeg\")"
+#define VER "47"
+#define VER_NAME "Roswell"
+#define VER_INFO "### Prerelease 47 (\"Roswell\")"
