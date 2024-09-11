2985bfd58189e6166a5830faeec2ba7779222efd
mooinglemur <mooinglemur@users.noreply.github.com>
2024-09-10 19:56:29 -0700

    [META] bump version for next (#297)

--- src/version.h
+++ src/version.h
@@ -2,6 +2,6 @@
 // Copyright (c) 2019, 2024 Michael Steil, et al
 // All rights reserved. License: 2-clause BSD
 
-#define VER "48"
-#define VER_NAME "Cadmium"
-#define VER_INFO "### Release 48 (\"Cadmium\")"
+#define VER "49"
+#define VER_NAME "next"
+#define VER_INFO "### Release 49 (\"next\")"
