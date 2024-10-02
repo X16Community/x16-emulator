27ed6ac655f0e153b9af6509e639bb24ca7fcaab
mooinglemur <mooinglemur@users.noreply.github.com>
2024-10-01 19:01:16 -0700

    fix for wasm

--- src/midi.c
+++ src/midi.c
@@ -128,10 +128,11 @@ struct midi_serial_regs
 struct midi_serial_regs mregs[2];
 static bool serial_midi_mutexes_initialized = false;
 
+void midi_serial_iir_check(uint8_t sel);
+
 #ifndef __EMSCRIPTEN__
 
 int handle_midi_event(void* data, fluid_midi_event_t* event);
-void midi_serial_iir_check(uint8_t sel);
 
 static uint8_t sysex_buffer[1024];
 static int sysex_bufptr;
