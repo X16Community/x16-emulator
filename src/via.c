9b1e9f9b07c08cfaca4f831327e42f87e3be3bcd
Catharine Manning <2330+catharinejm@users.noreply.github.com>
2025-05-06 10:08:55 -0400

    Writing to 65c22 IFR clears any flag specified in the written byte
    This matches the behavior of the hardware. Previous implementation
    only cleared CA2 or CB2 flags, and then only if they were configured
    as independent interrupts.

--- src/via.c
+++ src/via.c
@@ -132,13 +132,7 @@ via_write(via_t *via, uint8_t reg, uint8_t value)
 			via->registers[10] = value;
 			break;
 		case 13: // IFR
-			pcr = via->registers[12];
-			if ((value & 0x01) && ((pcr & 0b00001010) == 0b00000010)) {
-				via->registers[13] &= ~0x01;
-			}
-			if ((value & 0x08) && ((pcr & 0b10100000) == 0b00100000)) {
-				via->registers[13] &= ~0x08;
-			}
+			via->registers[13] &= ~value;
 			break;
 		case 14: // IER
 			if (value & 0x80) {
